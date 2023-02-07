546e3afc19aabe3fb8dbf2ac5e60fe5424664242
Alex <AlexanderTonn@gmx.de>
2023-02-07 11:36:29 +0100

    Update DataLogger.ino

--- SecureDigitalMemoryCard/SD_H/DataLogger.ino
+++ SecureDigitalMemoryCard/SD_H/DataLogger.ino
@@ -1,6 +1,23 @@
+// ###########
+// How to wire SD Card module
+// ###########
+/*
+                Arduino UNO     Arduino MEGA      Arduino MKR      
+VCC             3.3V or 5V      3.3V or 5V  	    3.3V 
+CS (SS)         4               53                7
+MOSI(COPI)      11              51                8
+CLK(SCK)        13              52                9
+MISO(CIPO)      12              50                10  
+GND             GND             GND               GND
+*/
+
+
+
+// ###################
+
   #include <SD.h>      // https://www.arduino.cc/reference/en/libraries/sd/
   #include <SPI.h>     // https://www.arduino.cc/reference/en/language/functions/communication/spi/
-  const int iSpi_SS = 53;  // SPI SS Pin (Chip Select)
+  const int iSpi_SS = 53;  // SPI SS Pin (CS == Chip Select / SS == Slave Select)
 
   File sdFile;                            // File object
   bool xSdAvailable = false; 
@@ -9,6 +26,16 @@
   byte byValue1 = 10; 
   byte byValue2 = 25; 
 
+
+void setup() {
+  pinMode(iSpi_SS, OUTPUT);
+  sdInit();
+}
+
+void loop() {
+ sdDataLogger(); 
+ delay(1000);
+}
 
 
 void sdDataLogger(){
